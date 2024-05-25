a9814e5f6a76b05ca99fa087fb3cf8ce5fbfe3ca
koubaahm <ahmed.koubaa@univ-rouen.fr>
2024-05-25 16:24:09 +0200

    initialisation micro service

---
+++ src/main/java/univ_rouen/fr/Music_Service/MusicServiceApplication.java
@@ -0,0 +1,13 @@
+package univ_rouen.fr.Music_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicServiceApplication.class, args);
+	}
+
+}
